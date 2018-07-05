762e4f7b5a4042b5839f6c03b5a837f6fe4149ac
vinaysowmya <vinaysowmya.saragadam@gmail.com>
2018-07-05 10:21:04 +0530

    power of two

---
+++ src/com/jda/functionalPrograms/PowerOfTwo.java
@@ -0,0 +1,15 @@
+package com.jda.functionalPrograms;
+
+import com.jda.utility.utility;
+
+public class PowerOfTwo {
+	public static void main(String args[])
+	{
+		System.out.println("Enter  the value of N");
+		utility util=new utility();
+		int num=util.inputInteger();
+		util.power(num);
+		
+	}
+
+}
